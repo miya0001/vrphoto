73824148138801f58a8db898fb488eb5c0f53fe0
miya0001 <miya@wpist.me>
2015-10-28 22:23:11 +0900

    resize image with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var gulp = require( 'gulp' );
-var download = require("gulp-download");
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var download = require( "gulp-download" );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+var data = require( 'gulp-data' );
+var exif = require( 'gulp-exif' );
+var extend = require( 'gulp-extend' );
+var imageResize = require( 'gulp-image-resize' );
 
 gulp.task( 'download', function () {
     return download( [
@@ -15,9 +19,58 @@ gulp.task( 'download', function () {
         .pipe( concat( 'three-plugins.min.js' ) )
         .pipe( uglify() )
         .pipe( gulp.dest( 'lib' ) );
-});
+} );
 
-gulp.task( 'default', [ 'download' ], function () {
+function gpsDecimal( direction, degrees, minutes, seconds ) {
+    var d = degrees + minutes / 60 + seconds / ( 60 * 60 );
+    return ( direction === 'S' || direction === 'W' ) ? d *= -1 : d;
+}
+
+gulp.task( 'exif', function () {
+    return gulp.src( './img/photo.jpg' )
+        .pipe( exif() )
+        .pipe( data( function ( file ) {
+            var filename = file.path.substring( file.path.lastIndexOf( '/' ) + 1 ),
+                exif = file.exif.gps,
+                calcLat = gpsDecimal.bind( null, exif.GPSLatitudeRef ),
+                calcLng = gpsDecimal.bind( null, exif.GPSLongitudeRef ),
+                data = {};
+            data[filename] = {
+                lat: calcLat.apply( null, exif.GPSLatitude ),
+                lng: calcLng.apply( null, exif.GPSLongitude ),
+                alt: exif.GPSAltitude,
+                dir: exif.GPSImageDirection
+            };
+            file.contents = new Buffer( JSON.stringify( data ) );
+        } ) )
+        .pipe( extend( 'gps.json', true, '    ' ) )
+        .pipe( gulp.dest( './' ) );
+} );
+
+gulp.task( 'exifshort', function () {
+    return gulp.src( './img/photo.jpg' )
+        .pipe( exif() )
+        .pipe( data( function ( file ) {
+            var filename = file.path.substring( file.path.lastIndexOf( '/' ) + 1 ),
+                data = {};
+            data[filename] = file.exif.gps;
+            file.contents = new Buffer( JSON.stringify( data ) );
+        } ) )
+        .pipe( extend( 'gpsshort.json', true, '    ' ) )
+        .pipe( gulp.dest( './' ) );
+} );
+
+gulp.task( 'resize', ['exif'], function () {
+    gulp.src( 'panorama/photo.jpg' )
+        .pipe( imageResize( {
+            width : 2048,
+            crop : false,
+            upscale : false
+        } ) )
+        .pipe( gulp.dest( 'img' ) );
+} );
+
+gulp.task( 'default', [ 'download', 'exif', 'resize' ], function () {
     return gulp.src( [
             'node_modules/three/three.min.js',
         ] )
