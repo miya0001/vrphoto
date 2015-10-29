ac614494ab8693b143283ad0d281304ff94322f1
miya0001 <miya@wpist.me>
2015-10-29 18:02:55 +0900

    fix textloader warning

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.body.appendChild( renderer.domElement );
 
 var scene, camera, controls, effect, manager;
 scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100 );
 controls = new THREE.VRControls( camera );
 effect = new THREE.VREffect( renderer );
 effect.setSize( window.innerWidth, window.innerHeight );
@@ -22,10 +22,11 @@ manager = new WebVRManager( renderer, effect, { hideButton: false } );
 init();
 
 function init() {
+    var texloader = new THREE.TextureLoader();
     var sphere = new THREE.Mesh(
         new THREE.SphereGeometry( 100, 20, 20 ),
         new THREE.MeshBasicMaterial( {
-            map: THREE.ImageUtils.loadTexture( 'img/photo.jpg' )
+            map: texloader.load( 'img/photo.jpg' )
         } )
     );
     sphere.scale.x = -1;
